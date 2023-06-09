d79ae5dceb13c70ab8768c1869e78455ef0dd784
Abhishek-Baksi <abhisek.baksi@gmail.com>
2023-06-09 14:37:27 +0530

    updated sserver.js for CORS policy

--- server.js
+++ server.js
@@ -1,27 +1,32 @@
-
-const express = require('express')
+const express = require("express");
 const path = require("path");
-const cors = require('cors');
-const app = express()
+const cors = require("cors");
+const app = express();
 
 // #############################################################################
 // This configures static hosting for files in /public that have the extensions
 // listed in the array.
 var options = {
-  dotfiles: 'ignore',
+  dotfiles: "ignore",
   etag: false,
-  extensions: ['htm', 'html','css','js','ico','jpg','jpeg','png','svg'],
-  index: ['index.html'],
-  maxAge: '1m',
-  redirect: false
-}
+  extensions: ["htm", "html", "css", "js", "ico", "jpg", "jpeg", "png", "svg"],
+  index: ["index.html"],
+  maxAge: "1m",
+  redirect: false,
+};
 app.use(cors({
   origin: 'https://fine-ruby-chinchilla-cuff.cyclic.app'
 }));
-app.use(express.static('build', options))
 
-const port = process.env.PORT || 3000
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Serve the index.html file for all requests
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`React app listening at http://localhost:${port}`)
-})
+  console.log(`React app listening at http://localhost:${port}`);
+});
